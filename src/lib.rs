e79cd0d1ce7fb21f60ba04a39479ef89d2d42eba
Greg Manning <1396183+tocklime@users.noreply.github.com>
2019-11-29 00:25:53 +0000

    day 4 part 1

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,7 @@ extern crate aoc_runner_derive;
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
+mod utils;
 
 aoc_lib! { year = 2018 }
---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+//use std::default::Default;
+use std::ops::AddAssign;
+
+pub fn de_prefixsum<T: AddAssign + Default + Copy>(input: &[T]) -> Vec<T> {
+    let mut total: T = Default::default();
+    let mut ans = Vec::with_capacity(input.len());
+    for i in input {
+        total += *i;
+        ans.push(total);
+    }
+    return ans;
+}
